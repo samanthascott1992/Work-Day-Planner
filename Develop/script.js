7e28b7bd174664f9f06decf8cc0d0bcd47407d6f
Samantha Scott <Samanthascott1992@gmail.com>
2020-04-05 17:15:06 -0600

    time coloring

--- Develop/script.js
+++ Develop/script.js
@@ -7,11 +7,6 @@ console.log(moment().format("MMM Do YY"));
 console.log(moment().format("hh : mm :  ss"));
 
 
-$( ".saveEvent" ).click(function() {
-    var hour = $(this).attr('id');
-    console.log("hour: " + hour);
-});
-
 function startTime() {
     document.getElementById('currentDay').innerHTML = moment().format("MMM Do YY") 
         + "<br>"
@@ -19,28 +14,43 @@ function startTime() {
     var t = setTimeout(startTime, 500);
 };
 
-// function timePassed(item){
-//     if(item.attr('id').replace("save","") < currentHour){ item.style = 'background-color: #h44444'; }
-// }  
-
-$( ".hour").ready(function() {
+$( ".hour" ).ready(function() {
     var hours = document.getElementsByClassName('hour');
     var currentHour = moment().format("k");
-    console.log("hours: " + hours);
     
     for(var i = hours.length ; i < hours.length+9 ; i++){
-        console.log("hours.namedItem(" + i +"):" + hours.namedItem(i).innerText);
-        console.log("Comparing i: " + i + " to currentHour: " + currentHour);
-        if(i <= currentHour){
-            hours.namedItem(i).style.backgroundColor = "teal";
-            //hours.namedItem(i).
+        if(i < currentHour){
+            hours.namedItem(i).style.backgroundColor = "#FF99CC";
+        }
+        else if(i == currentHour){
+            hours.namedItem(i).style.backgroundColor = "#FFCC00";
         }
         else{
-            hours.namedItem(i).style.backgroundColor = "green";
+            hours.namedItem(i).style.backgroundColor = "#66CC00";
         }
     }
-    //     if(hours[i].attr('id').replace("save","") < currentHour) {
-    //         hours[i].style = "background-color: 000";
-    //     }
-    // }
+
+    $( ".saveEvent").click(function(){
+        var hour = $(this).attr('id');
+        console.log("Hour: " + hour);
+    });
+});
+
+$( ".hourEvent" ).ready(function() {
+    var events = document.getElementsByClassName('hourEvent');
+    for(var i = 0; i < events.length; i++){
+        events.item(i).innerHTML = "Click to edit...";
+    }
+
+    $( ".hourEvent" ).click(function() {
+        console.log("In click event...");
+        $( this ).display = "none";
+        console.log("this" + this);
+        console.log("this.innerHTML" + this.innerHTML);
+        if( this.innerHTML == "Click to edit..."){
+            var newInput = document.createElement("INPUT");
+            newInput.attributes("id=" + $(this).attr('id'));
+            $( this ).append(newInput);
+        }
+    })
 });
